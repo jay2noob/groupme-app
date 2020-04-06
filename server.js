426b98888daf8219e7b0ea273824574e6f7a9b25
John Fields <jjfields24@outlook.com>
2020-04-06 04:29:07 -0400

    added new folders

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const morgan = require("morgan");
+//const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(morgan("dev"));
+//app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
