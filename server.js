1001f061f41f99ceb6aa027e929a0fe6bec41a65
John Fields <jjfields24@outlook.com>
2020-04-06 22:29:49 -0400

    cleaned up server.js

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 const express = require("express");
-//const morgan = require("morgan");
-const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3001;
+const connectDB = require("./config/db");
 
 const app = express();
 
-//app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
+// Connect DB
+connectDB();
 
-mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true
-  })
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+app.get("/", (req, res) => res.send("API running..."));
 
-//app.use(require("./routes/api-routes"));
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
