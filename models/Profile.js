55aaf593c0617eb2706eca1cd21280d919c289a1
Lorna Lynch <lornalynchit@yahoo.com>
2020-04-09 19:20:37 -0400

    updated profile model

---
+++ models/Profile.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'city',
+    required: true,
+  },
+  gender: {
+    type: String,
+  },
+  birthdate: {
+    type: String,
+    required: false
+  },
+  street_address: {
+    type: String,
+    required: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
