b0d1d0a130d1e6c004eba4f718738c821b157c7c
Joseph Lynn <josephlynn94@gmail.com>
2020-04-11 10:43:49 -0400

    added welcome route

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Welcome from './pages/Welcome'
 import Dashboard from './pages/Dashboard'
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <div className="App">
         <Switch>
           <Route exact path='/'>
+            <Welcome />
+          </Route>
+          <Route exact path='/dashboard'>
             <Dashboard />
           </Route>
         </Switch>
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import DashboardNavbar from '../components/DashboardNavbar'
+import SideNavbar from '../components/SideNavbar'
+
 
 function Dashboard() {
   return (
     <header>
       <DashboardNavbar />
+      <SideNavbar />
     </header>
   )
 }
