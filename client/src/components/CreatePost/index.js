bfc9a993abfdc140b33e8256a0bc9ba5376ee00e
Joe Lynn <josephlynn94@gmail.com>
2020-04-15 00:57:51 -0400

    created initial post component

---
+++ client/src/components/CreatePost/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles.css'
+
+function CreatePost() {
+  return (
+    <div className="create-post-container">
+      <div className="create-post-top">
+
+      </div>
+      <div className="create-post-body">
+
+      </div>
+      <div className="create-post-footer">
+
+      </div>
+    </div>
+  )
+}
+
+export default CreatePost
