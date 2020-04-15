7f81fd835cdaebb969e281e2f46d0ccf4e2a6eb8
Joe Lynn <josephlynn94@gmail.com>
2020-04-14 23:57:00 -0400

    added image to li in the comments

--- client/src/components/GroupComments/index.js
+++ client/src/components/GroupComments/index.js
@@ -5,11 +5,23 @@ import './styles.css'
 function GroupComments() {
   return (
     <div className="group-comment-top">
-      <div className="group-comment-img-container">
-        <img className="group-comment-img" src="../images/portrait.png" alt=""/>
-      </div>
       <ul className="group-comments-post">
         <li className="group-comment-li">
+          <div className="group-comment-img-container">
+            <img className="group-comment-img" src="../images/portrait.png" alt=""/>
+          </div>
+          <div className="group-comment-info">
+            <Link to="/profile">
+              <h6 className="group-comment-heading">USERNAME</h6>
+            </Link>
+            <p className="group-comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncu</p>
+          </div>
+        </li>
+
+        <li className="group-comment-li">
+          <div className="group-comment-img-container">
+            <img className="group-comment-img" src="../images/portrait.png" alt=""/>
+          </div>
           <div className="group-comment-info">
             <Link to="/profile">
               <h6 className="group-comment-heading">USERNAME</h6>
