8493c5ef1d80eb5d1d5a3339d1fc7fad5c73ee62
Joe Lynn <josephlynn94@gmail.com>
2020-04-19 17:26:11 -0400

    created search input state

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 
 function Search() {
+  // Delcare a search state value
+  const [searchValue, setSearchValue] = useState('')
+
+  // Update the state from event.target.value (what the user types)
+  function handleSearchChange (event) {
+    console.log(event.target.value)
+    setSearchValue(event.target.value)
+  }
+
   return (
     <div className="search-container">
       <label className="search-icon-container">
@@ -12,6 +21,10 @@ function Search() {
         type="text" 
         placeholder="Search for a user" 
         name="search" 
+        // set value prop to the current state 
+        value={searchValue}
+        // event function that keeps the value up to date 
+        onChange={handleSearchChange}
       />
     </div>
   )
