81a940bc52b83e3455ffef16030ef41a464b9032
John Fields <jjfields24@outlook.com>
2020-04-25 19:19:53 -0400

    profile action/reducer

--- client/src/components/CreateGroupForm/CreateGroupForm.js
+++ client/src/components/CreateGroupForm/CreateGroupForm.js
@@ -1,110 +1,112 @@
-import React, { useState } from 'react'
-import { Redirect } from "react-router-dom"
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import { createGroup } from '../../actions/group'
+import { createGroup } from "../../actions/group";
 import PropTypes from "prop-types";
-import './styles.css'
+import "./styles.css";
 
 function CreateGroupForm({ createGroup, group }) {
   const [formData, setFormData] = useState({
-    name: '',
-    description: '',
-    groupImage: ''
-  })
+    name: "",
+    description: "",
+    groupImage: "",
+  });
 
-  const { name, description, groupImage } = formData
+  const { name, description, groupImage } = formData;
 
-  const onChange = event => {
+  const onChange = (event) => {
     if (event.target.files) {
       setFormData({
         ...formData,
-        groupImage: event.target.files[0]
-      })
+        groupImage: event.target.files[0],
+      });
     } else {
       setFormData({
         ...formData,
-        [event.target.name]: event.target.value
-      })
+        [event.target.name]: event.target.value,
+      });
     }
-  }
-  const onSubmit = event => {
-    event.preventDefault()
-    createGroup({ name, description, groupImage })
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    createGroup({ name, description, groupImage });
 
     setFormData({
-      name: '',
-      description: '',
-      groupImage: ''
-    })
-  }
+      name: "",
+      description: "",
+      groupImage: "",
+    });
+  };
 
   if (group) {
     console.log("Group", group);
-    return <Redirect
-      to={{
-        pathname: `/group/${group._id}`,
-        group
-      }}
-      />;
+    return (
+      <Redirect
+        to={{
+          pathname: `/group/${group._id}`,
+          group,
+        }}
+      />
+    );
   }
 
   return (
     <form onSubmit={(event) => onSubmit(event)}>
       <fieldset className="create-group-fieldset">
-      
-        <ul className="create-group-list"> 
-          <li>   
+        <ul className="create-group-list">
+          <li>
             <label>Group Name:</label>
-            <input 
-              type="text" 
-              placeholder="Group name here" 
-              id="name" 
-              name="name" 
+            <input
+              type="text"
+              placeholder="Group name here"
+              id="name"
+              name="name"
               value={name}
               onChange={(event) => onChange(event)}
             />
           </li>
-          <li>   
+          <li>
             <label>About Group:</label>
             <textarea
-              type="text" 
-              placeholder="A brief description of the group" 
-              id="description" 
+              type="text"
+              placeholder="A brief description of the group"
+              id="description"
               name="description"
               value={description}
               onChange={(event) => onChange(event)}
-            >
-            </textarea>
+            ></textarea>
           </li>
         </ul>
 
         <div className="create-group-img-container">
           <label htmlFor="">Optional: Add an image for the group</label>
-            <br />
-            <input 
-              type="file" 
-              accept=".png, .jpg, .jpeg" 
-              name="groupImage"
-              onChange={(event) => onChange(event)}
-              className=" btn btn-secondary btn-fileupload" 
-            />
+          <br />
+          <input
+            type="file"
+            accept=".png, .jpg, .jpeg"
+            name="groupImage"
+            onChange={(event) => onChange(event)}
+            className=" btn btn-secondary btn-fileupload"
+          />
         </div>
 
-        <button className="btn btn-primary create-group-btn">Create Group</button>
+        <button className="btn btn-primary create-group-btn">
+          Create Group
+        </button>
       </fieldset>
     </form>
-  )
+  );
 }
 
 CreateGroupForm.propTypes = {
   createGroup: PropTypes.func.isRequired,
   isAdmin: PropTypes.bool,
-  group: PropTypes.object
+  group: PropTypes.object,
 };
 
 const mapStateToProps = (state) => ({
   isAdmin: state.isAdmin,
-  group: state.createGroup.group
+  group: state.group,
 });
 
 export default connect(mapStateToProps, { createGroup })(CreateGroupForm);
--- client/src/components/EditProfileForm/index.js
+++ client/src/components/EditProfileForm/index.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import EditProfileFrom from './EditProfileForm'
-import EditProfileImg from './EditProfileImg'
-import './styles.css'
+import React from "react";
+import EditProfileForm from "./EditProfileForm";
+import EditProfileImg from "./EditProfileImg";
+import "./styles.css";
 
 function EditProfileContainer() {
   return (
-    // Edit profile 
+    // Edit profile
     <div className="edit-profile-card-container">
       <div className="edit-profile-card">
         <div className="edit-profile-heading">
           <h2>Personal Information</h2>
         </div>
         <EditProfileImg />
-        <EditProfileFrom />
+        <EditProfileForm />
       </div>
     </div>
-  )
+  );
 }
 
-export default EditProfileContainer
+export default EditProfileContainer;
