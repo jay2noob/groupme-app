4b7f5295115b8de7b02e045226a5b9d263d2c0f1
Joe Lynn <josephlynn94@gmail.com>
2020-04-26 10:47:09 -0400

    added nan package

--- client/src/components/Group/index.js
+++ client/src/components/Group/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './styles.css'
+import GroupHero from './GroupHero'
 import GroupPost from './GroupPost'
 import CreatePostCard from './CreatePostCard'
 
 function GroupsContainer() {
   return (
     <section className="groups-card-container">
+      <GroupHero />
       <CreatePostCard />
       <GroupPost />
     </section>
