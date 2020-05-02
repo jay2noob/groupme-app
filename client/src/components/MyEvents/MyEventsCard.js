107fc2a16a2f5aecf937a0f8e7b88141e3bc88ae
John Fields <jjfields24@outlook.com>
2020-05-02 00:57:04 -0400

    removed errors from events

--- client/src/components/MyEvents/MyEventsCard.js
+++ client/src/components/MyEvents/MyEventsCard.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { IMAGE_URL } from '../../utils/setAuthToken';
-import './styles.css'
+import React from "react";
+import { Link } from "react-router-dom";
+//import { IMAGE_URL } from "../../utils/setAuthToken";
+import "./styles.css";
 
 function MyEventsCard({ myEvent }) {
-
-  var res = myEvent.avatar.match(/^\/\//g);
-  const url = res == null ? `${IMAGE_URL}${myEvent.avatar}` : myEvent.avatar;
+  //var res = myEvent.avatar.match(/^\/\//g);
+  //const url = res == null ? `${IMAGE_URL}${myEvent.avatar}` : myEvent.avatar;
 
   return (
     <ul className="myevents-list">
       <li className="myevents-item">
         <div className="myevents-img-container">
-          <img className="myevents-img" src={url} alt=""/>
+          <img className="myevents-img" src={""} alt="" />
         </div>
         <div className="myevents-content">
           <h2>Group Name</h2>
@@ -21,13 +20,14 @@ function MyEventsCard({ myEvent }) {
           <p className="myevents-event-time">{myEvent.time}</p>
 
           <Link to={`/group/events/${myEvent._id}`}>
-            <button className="btn btn-secondary myevents-btn">View Event</button>
-          </Link>  
+            <button className="btn btn-secondary myevents-btn">
+              View Event
+            </button>
+          </Link>
         </div>
       </li>
     </ul>
-  )
+  );
 }
 
-
-export default MyEventsCard
+export default MyEventsCard;
--- client/src/components/MyEvents/index.js
+++ client/src/components/MyEvents/index.js
@@ -1,67 +1,84 @@
-import React, { useState, useEffect } from 'react'
-import { getMyEvents } from '../../actions/event'
+import React, { useState, useEffect } from "react";
+import { getMyEvents } from "../../actions/event";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import MyEventsCard from './MyEventsCard'
-import Spinner from '../Spinner/Spinner'
-import './styles.css'
+import MyEventsCard from "./MyEventsCard";
+import Spinner from "../Spinner/Spinner";
+import { getMyGroups } from "../../actions/group";
+import "./styles.css";
 
-function MyEventsContainer({ getMyEvents, myEvents }) {
-  const [loading, setLoading] = useState(true)
-  const [page, setPage] = useState(0)
-  const [nextPage, setNextPage] = useState(false)
+function MyEventsContainer({ getMyGroups, getMyEvents, event: { myEvents } }) {
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(0);
+  const [nextPage, setNextPage] = useState(false);
 
-  console.log(myEvents)
+  console.log(myEvents);
 
   useEffect(() => {
-    if(myEvents.length === 0) {
-      getMyEvents({ page })
-    }
+    getMyGroups({ page });
+    getMyEvents({ page });
 
-    setLoading(false)
+    setLoading(false);
+
     // use debounce function to limit the number of requests sent to server if the user scrolls up and down too quiclky
-    const onScroll = e => {
-      const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight
-      const body = document.body
-      const html = document.documentElement
-      const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
-      const windowBottom = windowHeight + window.pageYOffset
+    const onScroll = (e) => {
+      const windowHeight =
+        "innerHeight" in window
+          ? window.innerHeight
+          : document.documentElement.offsetHeight;
+      const body = document.body;
+      const html = document.documentElement;
+      const docHeight = Math.max(
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight
+      );
+      const windowBottom = windowHeight + window.pageYOffset;
       if (windowBottom >= docHeight && !nextPage) {
-        setNextPage(true)
+        setNextPage(true);
         setTimeout(async () => {
-          const updatePage = page + 1
+          const updatePage = page + 1;
           console.log("Bottom Reached", updatePage);
-          getMyEvents({ page: updatePage })
-          setPage(updatePage)
-          setNextPage(false)
-        }, 1000)
+          getMyEvents({ page: updatePage });
+          setPage(updatePage);
+          setNextPage(false);
+        }, 1000);
       } else {
-        console.log("Not Yet")
+        console.log("Not Yet");
       }
-    }
-    window.addEventListener("scroll", onScroll)
+    };
+    window.addEventListener("scroll", onScroll);
 
-    return () => window.removeEventListener("scroll", onScroll)
-  }, [myEvents, getMyEvents, nextPage, page])
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   return (
     <section className="myevents-card-container">
       <h2 className="myevents-heading">My Events</h2>
-      { loading ? <Spinner /> : null }
-      {!loading && myEvents && myEvents.length === 0 ? (<h3>No Events to show</h3>) : (
-        myEvents.map(myEvent => <MyEventsCard myEvent={myEvent} key={myEvent._id} />)
+      {loading ? <Spinner /> : null}
+      {!loading && myEvents ? (
+        <h3>No Events to show</h3>
+      ) : (
+        myEvents.map((myEvent) => (
+          <MyEventsCard myEvent={myEvent} key={myEvent._id} />
+        ))
       )}
     </section>
-  )
+  );
 }
 
 MyEventsContainer.propTypes = {
   getMyEvents: PropTypes.func,
-  myEvents: PropTypes.array
-}
+  getMyGroups: PropTypes.func.isRequired,
+  event: PropTypes.object,
+};
 
 const mapStateToProps = (state) => ({
-  myEvents: state.group.myEvents
-})
+  event: state.event,
+});
 
-export default connect(mapStateToProps, { getMyEvents })(MyEventsContainer);
+export default connect(mapStateToProps, { getMyEvents, getMyGroups })(
+  MyEventsContainer
+);
