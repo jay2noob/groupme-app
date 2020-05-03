5a7f31ae23d0a4e194eb27d409108c309fe3d38e
John Fields <jjfields24@outlook.com>
2020-05-02 22:21:59 -0400

    final commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 const bodyParser = require("body-parser");
 global.__basedir = __dirname;
 global.pageOptions = {
-  limit: 15 // total number of items
-}
+  limit: 15, // total number of items
+};
 
 // Connect DB
 connectDB();
@@ -18,9 +19,7 @@ app.use(
     extended: true,
   })
 );
-app.use('/static', express.static('public'))
-
-app.get("/", (req, res) => res.send("API running..."));
+app.use("/static", express.static("public"));
 
 // Define routes
 app.use("/api/users", require("./routes/api/users"));
@@ -29,6 +28,16 @@ app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/events", require("./routes/api/events"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/groups", require("./routes/api/groups"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 3001;
 
